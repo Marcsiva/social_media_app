528825829e929729f08b05cc88048042f06b5834
Marcsiva <sivakumar20011005@gmail.com>
2023-11-12 17:51:24 +0530

    login and user profile

---
+++ lib/model/user_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
+      id: json['id'] as String?,
+      uid: json['uid'] as String,
+      username: json['username'] as String,
+      email: json['email'] as String,
+      image: json['image'] as String?,
+      description: json['description'] as String?,
+      phonenumber: json['phonenumber'] as String,
+    );
+
+Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
+      'id': instance.id,
+      'uid': instance.uid,
+      'username': instance.username,
+      'email': instance.email,
+      'image': instance.image,
+      'description': instance.description,
+      'phonenumber': instance.phonenumber,
+    };
